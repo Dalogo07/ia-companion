a5f3e64433ed0822168bbd9d9de78e9f4a71521c
Dalogo07 <david.lopez.matematicas@gmail.com>
2026-05-27 02:45:37 +0200

    test change

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-return {"message": "CAMBIO DE PRUEBA 123"}
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
